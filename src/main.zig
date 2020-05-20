73b01833913f936fe98bc14954dc9e32bd014093
Marcus Stojcevich <mstojcevich@duosecurity.com>
2020-05-19 20:13:56 -0400

    Some SDL stuff

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,45 @@
+const c = @cImport({
+    @cInclude("SDL2/SDL.h");
+});
 const bus = @import("bus.zig");
 
-pub fn main() void {
+const SDL_WINDOWPOS_UNDEFINED = @bitCast(c_int, c.SDL_WINDOWPOS_UNDEFINED_MASK);
+
+extern fn SDL_PollEvent(event: *c.SDL_Event) c_int;
+
+pub fn main() !void {
+    if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
+        c.SDL_Log("Unable to initialize SDL: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    }
+    defer c.SDL_Quit();
+
+    const screen = c.SDL_CreateWindow("zig-nes", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 256, 240, c.SDL_WINDOW_OPENGL) orelse {
+        c.SDL_Log("Unable to create window: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    };
+    defer c.SDL_DestroyWindow(screen);
+
+    const renderer = c.SDL_CreateRenderer(screen, -1, 0) orelse {
+        c.SDL_Log("Unable to create renderer: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    };
+    defer c.SDL_DestroyRenderer(renderer);
+
+    var quit = false;
     bus.run();
+    while (!quit) {
+        var event: c.SDL_Event = undefined;
+        while (SDL_PollEvent(&event) != 0) {
+            switch (event.@"type") {
+                c.SDL_QUIT => {
+                    quit = true;
+                },
+                else => {},
+            }
+        }
+        c.SDL_RenderPresent(renderer);
+
+        c.SDL_Delay(17);
+    }
 }
